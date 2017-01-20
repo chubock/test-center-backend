94c10feff922ff6d6ab20d0b1d9353f66183c37b
yubar persia <yubar45@gmail.com>
2017-01-21 02:17:06 +0330

    service class FileService removed. service class ManagerService created.

---
+++ src/main/java/com/ztc/testcenter/service/ManagerService.java
@@ -0,0 +1,41 @@
+package com.ztc.testcenter.service;
+
+import com.ztc.testcenter.domain.File;
+import com.ztc.testcenter.domain.question.Question;
+import com.ztc.testcenter.repository.FileRepository;
+import com.ztc.testcenter.repository.question.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by Yubar on 1/19/2017.
+ */
+
+@Service
+@Transactional
+public class ManagerService {
+
+    private final QuestionRepository questionRepository;
+    private final FileRepository fileRepository;
+
+    @Autowired
+    public ManagerService(QuestionRepository questionRepository, FileRepository fileRepository) {
+        this.questionRepository = questionRepository;
+        this.fileRepository = fileRepository;
+    }
+
+    public File saveFile(File file) {
+        return fileRepository.save(file);
+    }
+
+    public Question saveQuestion(Question question) {
+        question.prepare();
+        return questionRepository.save(question);
+    }
+
+    public void deleteQuestion(long id) {
+        questionRepository.delete(questionRepository.getOne(id));
+    }
+
+}
