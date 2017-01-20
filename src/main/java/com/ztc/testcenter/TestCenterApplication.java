8684af6e7975d5c6046b414a6c5f87c03fae9d31
yubar persia <yubar45@gmail.com>
2017-01-21 02:19:56 +0330

    TestCenterApplication updated to allow cors requests.

--- src/main/java/com/ztc/testcenter/TestCenterApplication.java
+++ src/main/java/com/ztc/testcenter/TestCenterApplication.java
@@ -2,6 +2,10 @@ package com.ztc.testcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by Yubar on 1/15/2017.
@@ -9,7 +13,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestCenterApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TestCenterApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*");
+            }
+        };
     }
 }
