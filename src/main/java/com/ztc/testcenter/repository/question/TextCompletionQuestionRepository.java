4640a5629a5a58cc0e40f299396a227250f418b2
yubar persia <yubar45@gmail.com>
2017-01-24 07:28:53 +0330

    method findAllWithItems added to TextCompletionQuestionRepository class.

--- src/main/java/com/ztc/testcenter/repository/question/TextCompletionQuestionRepository.java
+++ src/main/java/com/ztc/testcenter/repository/question/TextCompletionQuestionRepository.java
@@ -2,10 +2,19 @@ package com.ztc.testcenter.repository.question;
 
 import com.ztc.testcenter.domain.question.SelectInPassageQuestion;
 import com.ztc.testcenter.domain.question.TextCompletionQuestion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Created by Yubar on 1/19/2017.
  */
 public interface TextCompletionQuestionRepository extends JpaRepository<TextCompletionQuestion, Long> {
+
+    @EntityGraph(attributePaths = "items")
+    @Query(value = "select q from TextCompletionQuestion q")
+    Page<TextCompletionQuestion> findAllWithItems(Pageable pageable);
+
 }
