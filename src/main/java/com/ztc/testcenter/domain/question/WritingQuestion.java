37b29f2171920e4364244fd7cae16bed0fb010e6
yubar persia <yubar45@gmail.com>
2017-02-14 19:44:22 +0330

    taskType property added to WritingQuestion and WritingQuestionDTO

--- src/main/java/com/ztc/testcenter/domain/question/WritingQuestion.java
+++ src/main/java/com/ztc/testcenter/domain/question/WritingQuestion.java
@@ -1,7 +1,10 @@
 package com.ztc.testcenter.domain.question;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by Yubar on 1/18/2017.
@@ -10,4 +13,22 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue("WRITING")
 public class WritingQuestion extends Question {
+
+    private TaskType taskType;
+
+    @Enumerated
+    @NotNull
+    @Column(nullable = false)
+    public TaskType getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(TaskType taskType) {
+        this.taskType = taskType;
+    }
+
+    public static enum TaskType {
+        ANALYZE_AND_ISSUE,
+        ANALYZE_AND_ARGUMENT
+    }
 }
--- src/main/java/com/ztc/testcenter/dto/question/WritingQuestionDTO.java
+++ src/main/java/com/ztc/testcenter/dto/question/WritingQuestionDTO.java
@@ -7,11 +7,32 @@ import com.ztc.testcenter.domain.question.WritingQuestion;
  */
 public class WritingQuestionDTO extends QuestionDTO {
 
+    private WritingQuestion.TaskType taskType;
+
+    public WritingQuestion.TaskType getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(WritingQuestion.TaskType taskType) {
+        this.taskType = taskType;
+    }
+
+    public WritingQuestion convert(WritingQuestion question) {
+        super.convert(question);
+        question.setTaskType(getTaskType());
+        return question;
+    }
+
     @Override
     public WritingQuestion convert() {
         WritingQuestion question = new WritingQuestion();
         convert(question);
         return question;
+    }
+
+    public void copy(WritingQuestion question) {
+        super.copy(question);
+        setTaskType(question.getTaskType());
     }
 
     public static WritingQuestionDTO valueOf(WritingQuestion question) {
